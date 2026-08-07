0a91ef679a4240b02b09cbf9f5d76a575039ebd4
Nikola Jambresic <nikolajambresic8@gmail.com>
2026-08-07 10:40:13 +0200

    Initial commit

---
+++ LapLoggerApp/src/main/java/hr/tvz/laploggerapp/LapLoggerAppApplication.java
@@ -0,0 +1,13 @@
+package hr.tvz.laploggerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapLoggerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LapLoggerAppApplication.class, args);
+    }
+
+}
---
+++ LapLoggerApp/src/test/java/hr/tvz/laploggerapp/LapLoggerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.laploggerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapLoggerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
